2539f9d017341bfb72e557991a32e3e23482f990
Maxime Peybernes <maxime.peybernes@gmail.com>
2024-11-18 15:28:05 +0100

    a-b-c

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,11 +4,16 @@ public class Humain {
 	private String nom;
 	private String boissonFav; 
 	private int argent;
+	private int nbConnaissances = 0;
+	private Humain[] memoire = new Humain[30];
 	
 	public Humain(String pfnom, String pfBoisson, int pfargent) {
 		nom = pfnom;
 		boissonFav = pfBoisson;
 		argent = pfargent;
+		for (int i=0;i<memoire.length; i++){
+			memoire[i]=null;
+		}
 	}
 	
 	
@@ -39,11 +44,11 @@ public class Humain {
 	
 	public void acheter(String bien, int prix) {
 		if(argent>=prix) {
-			parler("J'ai "+argent+" sous en poche. Je vais pouvoir m'offrir un "+bien+" à "+prix+" sous.");
+			parler("J'ai "+argent+" sous en poche. Je vais pouvoir m'offrir un "+bien+" ï¿½ "+prix+" sous.");
 			perdreArgent(prix);
 		}
 		else {
-			parler("Je n'ai plus que "+argent+" sous en poche. Je ne peux même pas m'offrir un "+bien+" à "+prix+" sous.");
+			parler("Je n'ai plus que "+argent+" sous en poche. Je ne peux mï¿½me pas m'offrir un "+bien+" ï¿½ "+prix+" sous.");
 		}
 	}
 	
@@ -53,6 +58,39 @@ public class Humain {
 	
 	protected void perdreArgent(int perte) {
 		argent-=perte;
+	}
+
+
+	public void faireConnaissanceAvec(Humain newH){
+		direBonjour();
+		newH.repondre(this);
+		memoriser(newH);
+	}
+
+	private void memoriser(Humain newH){
+		if (nbConnaissances<memoire.length){
+			memoire[nbConnaissances] = newH;
+			nbConnaissances++;
+		} else{
+			nbConnaissances = 0;
+			memoriser(newH);
+		}
+	}
+
+	private void repondre(Humain qqun){
+		direBonjour();
+		memoriser(qqun);
+	}
+
+
+	public void listerConnaissance(){
+		String txt ="Je connais beaucoup de monde dont : ";
+		for (int i=0;i<memoire.length;i++){
+			if(memoire[i]!=null){
+				txt.concat(memoire[i].getNom() + "  ");
+			}
+		}
+		parler(txt);
 	}
 
 
